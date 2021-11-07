5ceb0d052fded080528b6415d03007e171f032c3
ShaolunWang <60100813+ShaolunWang@users.noreply.github.com>
2021-11-07 14:49:03 +0100

    Add files via upload

---
+++ zsh-source.zsh
@@ -0,0 +1,5 @@
+source ~/.zsh-local/zsh-z/zsh-z.plugin.zsh
+source ~/.zsh-local/zsh-local/key-bindings.zsh
+source ~/.zsh-local/zsh-local/zsh_local.zsh
+source ~/.zsh-local/zsh-titles/titles.plugin.zsh
+source ~/.zsh-local/zsh-autosuggestions/zsh-autosuggestions.zsh
